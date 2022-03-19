3a4cc0d37fed77211ee379f24d4555ffefcfb39f
ricc4rdop <pbricar@gmail.com>
2022-03-19 20:42:27 -0300

    Se agrega css y js

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+
+/* Desplazamiento vertical suave */
+
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+
+/* Tooltip  para formulario de contacto */
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
